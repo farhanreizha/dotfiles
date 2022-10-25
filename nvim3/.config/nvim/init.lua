515e85bbbddbcc052772b6b59e42330be79a0153
farhanreizha <reizha226@gmail.com>
2022-10-25 21:07:11 +0700

    back to astrovim

---
+++ nvim3/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+require('core/options')
+require('core/mappings')
+require('plugins')
+require("configs/treesitter")
+require("configs/lualine")
+require("configs/bufferline")
+require("configs/nvim-tree")
+
